e0bebf0fcc5eceb6a22e073c5409628c11b82245
elgaizzaa <161436093+elgaizzaa@users.noreply.github.com>
2024-05-14 15:03:20 +0700

    menambahkan fungsi member statik

--- mbrstaticfungction.cpp
+++ mbrstaticfungction.cpp
@@ -1,3 +1,28 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+class mahasiswa
+{
+    private:
+        static int nim;
+
+    public:
+        int id;
+        string nama;
+
+    void setID();
+    void printAll();
+    static void setNim(intpNim) { nim = pNim; }
+    static int getNim() { return nim; }
+
+    mahasiswa(string pnama)
+    {
+            nama = pnama;
+            setID();
+
+    }
+};
+
+int mahasiswa::nim = 0;
+
