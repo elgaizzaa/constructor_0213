dc78ae935c996371b62978da3229b27b1a9e2841
elgaizzaa <161436093+elgaizzaa@users.noreply.github.com>
2024-05-14 15:49:22 +0700

    isi kelas angka

--- destrrucktor.cpp
+++ destrrucktor.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 using namespacse std;
 
-class angka{
+class angka{ 
+    private:
+    int *arr;
+    int panjang;
+    public:
+    angka(int); //constructor
+    ~angka(); //destructor
+    void cetakData();
+    void isiData();
+
+};
+// definisi member fungction
+
+angka::angka(int i){ // constructor
+    panjang = i;
+    arr = new int[i];
+    isiData();
    
 }
