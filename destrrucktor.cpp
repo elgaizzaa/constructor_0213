2f969ef17802a3755584bf2979a42d8f348fed7b
elgaizzaa <161436093+elgaizzaa@users.noreply.github.com>
2024-05-14 15:59:34 +0700

    menambahkan int main

--- destrrucktor.cpp
+++ destrrucktor.cpp
@@ -43,3 +43,12 @@ void angka::cetakData(){
         }
         cout << endl;
     }
+
+    int main(){
+        angka belajarcpp(3);
+        angka*ptrBelajarcpp = new angka (5);
+        delete ptrBelajarcpp;
+
+        return 0;
+        //destructor Dipanggil
+    }
